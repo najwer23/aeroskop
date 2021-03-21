d3cfa6851e2a603e8c23ea1cca29d30ab44effb9
Travis CI <najwer23@gmail.com>
2021-03-21 23:35:42 +0100

    Hamburger menu

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,20 +1,84 @@
-import React from 'react'
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../CSS/Nav.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMugHot } from '@fortawesome/free-solid-svg-icons'
 
+const menuRoutes = [
+    { path: "/", name: 'Home'},
+    // { path: "/test", component: Test, name: 'Test' }
+];
+
+
+class Hamburger extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        window.addEventListener("resize", this.menuMobileHide);
+    }
+    componentWillUnmount() {
+        window.addEventListener("resize", null);
+    }
+
+    menuMobileHide() {
+        const hamburgerBtn = document.querySelector('.hamburger-button');
+        const menuMobile = document.querySelector('.menu-mobile');
+        
+        if (window.innerWidth > 1000) {
+            document.body.classList.remove("scroll");
+            hamburgerBtn.classList.remove("hamburger-active");
+            menuMobile.classList.remove('menu-mobile-active');
+            hamburgerBtn.setAttribute('aria-expanded', hamburgerBtn.classList.contains('hamburger-active'));
+        }
+    }
+
+    menuMobileClick() {
+        const hamburgerBtn = document.querySelector('.hamburger-button');
+        const menuMobile = document.querySelector('.menu-mobile');
+
+        hamburgerBtn.classList.toggle('hamburger-active');
+        hamburgerBtn.setAttribute('aria-expanded', hamburgerBtn.classList.contains('hamburger-active'));
+        menuMobile.classList.toggle('menu-mobile-active');
+        document.body.classList.toggle('scroll');
+    }
+
+    render() {
+        return (
+            <div className="hamburger">
+                <button className="hamburger-button" onClick={this.menuMobileClick} aria-expanded="false">
+                    <span className="screen-read-only">Open / Close Menu</span>
+                    <span className="hamburger-box">
+                        <span className="hamburger-box-line"></span>
+                    </span>
+                </button>
+            </div>
+        )
+    }
+}
+
+class MobileMenu extends Hamburger {
+    render() {
+        return (
+            <div className="menu-mobile">   
+                <ul className="menu-mobile-links">
+                    <li>aeroskop</li>
+                    {menuRoutes.map((v, i) => <li onClick={this.menuMobileClick} key={i}><Link to={v.path}>{v.name}</Link></li>)}
+                    <li>Mariusz Najwer <br />2021</li>
+                </ul>
+            </div>
+        )
+    } 
+}
 
 export default function Nav() {
-    const menuRoutes = [
-        { path: "/", name: 'Home'},
-        // { path: "/test", component: Test, name: 'Test' }
-      ];
-
     return (
         <nav>
+            <MobileMenu />
             <div className="menu">
                 <div className="menu-box">
+                    <Hamburger />
                     <div className="menu-title"> <FontAwesomeIcon style={{ color: "orange" }} icon={faMugHot} size="lg" /> coffee4u </div>
                     <ul className="menu-list">
                         {menuRoutes.map((v, i) => <li key={i}><Link to={v.path}>{v.name}</Link></li>)}
