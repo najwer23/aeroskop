32367e8aabbe64342b579b8f51d7be758bc6cdcf
Travis CI <najwer23@gmail.com>
2021-03-23 06:21:12 +0100

    Button at bottom of tag <li>

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import '../CSS/Feed.css';
 
 export default function Feed() {
@@ -8,9 +9,15 @@ export default function Feed() {
                 <ul className="feed-box">
                     <li className="feed-box-item">
                         <img src={`${process.env.PUBLIC_URL}/assets/images/raya.jpg`} alt="Raya i ostatni smok - plakat" ></img>
-                        <div className="feed-box-item-desc">
-                            <h1>RAYA I OSTATNI SMOK</h1>
-                            <h3>Raya and the Last Dragon</h3>
+                        <div className="feed-box-item-desc atBottom">
+                            <div>
+                                <h1>RAYA I OSTATNI SMOK</h1>
+                                <h3>Raya and the Last Dragon</h3>
+                                <p>W zamieszkałym przez starożytną cywilizację królestwie Kumandra, wojowniczka Raya usiłuje odnaleźć ostatniego smoka.</p>
+                            </div>
+                            <div className="">
+                                <Link to={"/drinks"} ><div className='btn wave link'>Read more!</div></Link>
+                            </div>
                         </div>
                     </li>
                 </ul>
